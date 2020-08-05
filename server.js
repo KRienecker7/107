690faa3dc77aebace7611bea864eb860603895c1
KRienecker7 <66344839+KRienecker7@users.noreply.github.com>
2020-08-05 06:59:41 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var server = express();
 
 /*****************************************************************************/
 /**********Server Configuartion */
+/* win: ctr + p
 /*****************************************************************************/
 
 // render HTML from endpoints
@@ -11,18 +12,30 @@ server.set('views', __dirname + "/public");
 server.engine('html', ejs.renderFile);
 server.set('view engine', ejs);
 
+//Server Static files (js, css, img, pdf, doc, .... which allows server to access all files in public)
+server.use(express.static(__dirname + "/public"));
+
+
+// body-parse to read paylod (ajax data) directly to object
+var bparser = require('body-parser');
+server.use(bparser.json());
+
 
 /*****************************************************************************/
-/**********Web Server which sends the sintructions*/  
+/**********Web Server which sends the intructions*/  
 /*****************************************************************************/
 
 //THESE ARE AN ENDPOINTS
 server.get("/", function(req, res){
-    res.send("Hello There!");
+    res.render("index.html");
+});
+
+server.get("/admin", function(req, res){
+    res.render("admin.html");
 });
 
 server.get("/about", function(req, res){
-    res.send("about.html");
+    res.render("about.html");
 });
 
 server.get("/work", function(req, res){
@@ -37,11 +50,53 @@ server.get("/contact", function(req, res){
 /*****************************************************************************/
 /*******Rest API is where the end points that don't respond to html but work nire with end point i.e.json*/
 /*****************************************************************************/
-server.post('/api/item', function(req, res){
+
+var data = [];
+var cnt = 1;
+
+server.post('/api/items', function(req, res){
     //code here to save the item
+    var item = req.body;
+
+    if(!item.price){
+        res.status(400); //bad request
+        res.send("Price is required on the Item");
+    }
+
+    else{
+        data.push(item);
+        
+        item.id = cnt;
+        cnt +=1;
+        item.status = 'Saved!';
+
+        res.status(201); //created
+        res.json(item);
+    }
 });
 
 
+server.get('/test/1',(req, res) => {
+    //sole the problem and reply results
+
+    //data
+    var nums = [81,3,1,543,-2,53,-28,897123,1,2,-9487745, 99];
+
+    //problem: find the biggest number in the array
+    //your code
+    greater: function(81,3,1,543,-2,53,-28,897123,1,2,-9487745, 99){
+        var greater = Math.max(81,3,1,543,-2,53,-28,897123,1,2,-9487745, 99);  
+    }
+    //result
+
+    res.send("Res: " +3);
+});
+
+// > 3000
 server.listen(8080, function(){
     console.log("Server running at http://:localhost:8080");
 });
+
+//Rest API
+// Mongo DB and Mongoose
+// npm install --global nodemon
